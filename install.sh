af92498bfda09c6fb9b9b5e0b229c88ae7caf18e
Brian O'Mara <bomara512@gmail.com>
2023-11-05 18:50:55 -0600

    clean up

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@
 echo "Setting up your Mac..."
 
 # Check for Oh My Zsh and install if we don't have it
-if test ! $(which omz); then
-  /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/HEAD/tools/install.sh)"
+if test ! "$(which omz)"; then
+  /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/HEAD/tools/install.sh)" & wait
 fi
 
 # Check for powerlevel10k and install if we don't have it
@@ -13,12 +13,21 @@ if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k" ]; then
   git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 fi
 
-source ./brew.sh
+. ./brew.sh
 
 stow --verbose --target=$HOME --restow git p10k shell zsh
 
-pyenv install 3.11
-pyenv global 3.11
+PYTHON_VERSION="3.11";
+echo "PYTHON_VERSION = $PYTHON_VERSION";
+
+# shellcheck disable=SC2039
+if [[ $(pyenv global) != "$PYTHON_VERSION" ]]; then
+  echo "installing global python: $PYTHON_VERSION...";
+  pyenv install $PYTHON_VERSION;
+  pyenv global $PYTHON_VERSION;
+else
+  echo "global python is already at $PYTHON_VERSION";
+fi
 
 # Set macOS preferences - we will run this last because this will reload the shell
-source ./.macos
+. ./.macos
